effee37ad8bd43783fb96d8d2dbc8789cc4be971
Fistboy <2110680@studentmail.ucol.ac.nz>
2025-11-20 17:11:28 +1300

    feat: initial structure with domain models, config, and test scaffold

---
+++ VirtualSensor/Domain/SensorData.cs
@@ -0,0 +1,13 @@
+﻿namespace SensorSim.Domain
+{
+    public class SensorData
+    {
+        public int ReadingId { get; set; }
+        public string SensorName { get; set; }
+        public string Location { get; set; }
+        public double ValueC { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool IsValid { get; set; }
+        public bool IsAnomaly { get; set; }
+    }
+}
